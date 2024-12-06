e24f139ac15f8e78d3a18de86417287bd4ddb389
GiangHV <giang4451050083@st.qnu.edu.vn>
2024-12-06 09:09:37 +0700

    dfdfdfd

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,8 +15,8 @@ const Layout = () => {
     <div>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={`/${url}`} element={<App />} />
-          <Route path={`/${url}/house`} element={<ManageHouse />}>
+          {/* <Route path={`/${url}`} element={<App />} /> */}
+          <Route path={`/${url}`} element={<ManageHouse />}>
             <Route index element={<ListHouse />} />
             <Route path={`create`} element={<CreateNewHouse />} />
             <Route path={`update`} element={<UpdateHouse />} />

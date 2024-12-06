73eb9fd383784815c0215837871529b404617518
GiangHV <giang4451050083@st.qnu.edu.vn>
2024-12-06 08:37:03 +0700

    fix: update new url (layout)

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -9,20 +9,21 @@ import UpdateHouse from "./components/owner/house/UpdateHouse";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 
+const url = '77Home-FrontEnd'
 const Layout = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/house" element={<ManageHouse />}>
+          <Route path={`/${url}`} element={<App />} />
+          <Route path={`/${url}/house`} element={<ManageHouse />}>
             <Route index element={<ListHouse />} />
-            <Route path="create" element={<CreateNewHouse />} />
-            <Route path="update" element={<UpdateHouse />} />
-            <Route path=":id" element={<DetailHouse />} />
+            <Route path={`create`} element={<CreateNewHouse />} />
+            <Route path={`update`} element={<UpdateHouse />} />
+            <Route path={`:id`} element={<DetailHouse />} />
           </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path={`/${url}/login`} element={<Login />} />
+          <Route path={`/${url}/register`} element={<Register />} />
         </Routes>
       </Suspense>
     </div>
